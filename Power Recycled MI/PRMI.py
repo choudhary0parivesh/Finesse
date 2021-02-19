c38c892ff5ddf3264e71f7a26d90b30486560d8a
Parivesh Choudhary <choudhary0parivesh@gmail.com>
2021-02-19 11:40:03 +0530

    add core optics

---
+++ Power Recycled MI/PRMI.py
@@ -0,0 +1,16 @@
+from pykat import finesse        
+from pykat.commands import *    
+import numpy as np               
+import matplotlib.pyplot as plt  
+
+
+
+################ core optics #######################
+
+basecode = """
+
+
+
+
+
+"""
