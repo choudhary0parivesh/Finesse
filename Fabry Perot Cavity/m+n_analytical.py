a3ddded026b24d981a331e510b24466a6d9f026d
Parivesh Choudhary <choudhary0parivesh@gmail.com>
2021-07-22 00:22:20 +0530

    Add files via upload

--- Fabry Perot Cavity/m+n_analytical.py
+++ Fabry Perot Cavity/m+n_analytical.py
@@ -2,16 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
  
   
-m = np.arange(1.0,11.0,1)
-R = np.arange(30.0,35.0,0.01)
+m = np.arange(1.0,11.0,1)           #m modes
+R = np.arange(30.0,35.0,0.01)       #RoC mirror
   
 # Creating 2-D grid of features
 
-r1 = 0.998
+r1 = 0.998                          #R of mirror
 r2 = 0.990
-L = 9.1
-g = 1-(L/R)
-F = (np.pi*((r1*r2)**0.25))/(1-(r1*r2)**0.5)
+L = 9.1                             #length of cav                                                 
+g = 1-(L/R)                         #g
+F = (np.pi*((r1*r2)**0.25))/(1-(r1*r2)**0.5)    #Finesse
 
 print("F",F)
 #print("G",g)
@@ -23,9 +23,9 @@ fig, ax = plt.subplots(1,1)
 #Z=  1/(1+((2*F/np.pi)**2)*np.sin(X*np.arccos((1-L/Y)**0.5))**2)**0.5
 
 
+#relative to fun HOM
 
-
-Z= 1/((1+((2*F/np.pi)**2)*(np.sin(X*np.arccos((1-L/Y)**0.5)))**2)**0.5)
+Z= 1/((1+((2*F/np.pi)**2)*(np.sin(X*np.arccos((1-L/Y)**0.5)))**2)**0.5)  
 
 # plots filled contour plot
 cf = ax.contourf(X, Y, Z)
